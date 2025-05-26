f6e8b8a00ed253db9080a196215a64e9b1bd6350
abbas <abbas1795khan@gmail.com>
2025-05-26 10:43:05 +0500

    master commit

---
+++ MyPython.py
@@ -0,0 +1,7 @@
+
+from datetime import datetime
+def calculate_age(dob):
+    today = datetime.today()
+    dob = datetime.strptime(dob, '%Y-%m-%d')
+    age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+    return age
